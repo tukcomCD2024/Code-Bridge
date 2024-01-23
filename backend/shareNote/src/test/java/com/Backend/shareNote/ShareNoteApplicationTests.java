09b053f29ed5613bc1eda3b897d5ba196a53eb34
Kimsangwon <doncham98@gmail.com>
2024-01-23 12:40:53 +0900

    feat : 스프링 초기 설정

---
+++ backend/shareNote/src/test/java/com/Backend/shareNote/ShareNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.shareNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
