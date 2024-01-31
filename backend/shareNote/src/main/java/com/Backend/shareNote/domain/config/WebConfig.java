707bec7ccaef49a4356a4aa05915ef8b20f4d5b6
Kimsangwon <doncham98@gmail.com>
2024-01-31 22:26:19 +0900

    feat : CORS 설정 추가

---
+++ backend/shareNote/src/main/java/com/Backend/shareNote/domain/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.Backend.shareNote.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
