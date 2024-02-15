f0d5660140a00558262aa14574f58423f7e50150
가인철 <45278265+entellaKa@users.noreply.github.com>
2024-02-15 13:22:21 +0900

    delete tag include number

---
+++ AI/Preprocessing/TagSetting.py
@@ -0,0 +1,34 @@
+import os
+import csv
+
+
+def numCheck(tag):
+    for i in range(10):
+        if str(i) in tag:
+            return False
+    return True
+
+
+def includeNo():
+    f = open("../asset/tag/tags5차 작업.csv", "r")
+    g = open("../asset/tag/tags6차 작업.csv", "w", newline='\n')
+    read = csv.reader(f)
+    write = csv.writer(g)
+
+    for i in read:
+        if numCheck(i[0]):
+            write.writerow(i)
+
+
+def checkTagLength(l):
+    f = open("../asset/tag/tags5차 작업.csv", "r")
+    read = csv.reader(f)
+
+    arr = []
+    for i in read:
+        if len(i[0]) > l:
+            arr.append(i[0])
+
+    return arr
+
+includeNo()
