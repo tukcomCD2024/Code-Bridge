116945dc84fff3e331c08ee27f448c74d47b97f4
Ko SeongChan <shinjineunsu@gmail.com>
2024-02-19 22:21:57 +0900

    Fragment
    Fragment에서 Activity 화면 띄우기 구현 예정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.10.1")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
 
-    
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- android/app/src/main/java/com/example/sharenote/HomeFragment.kt
+++ android/app/src/main/java/com/example/sharenote/HomeFragment.kt
@@ -5,22 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sharenote.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var mBinding : FragmentHomeBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        /*
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        mBinding = binding
+        return mBinding?.root
+        */
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
 
 }
