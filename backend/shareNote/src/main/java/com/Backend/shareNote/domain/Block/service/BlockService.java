0fcd55e3d3c32a7db1852162a6d3eee6bf1af40e
Kimsangwon <doncham98@gmail.com>
2024-02-21 19:48:16 +0900

    feat : block 조회 로직 수정
    block 조회 시 block의 blockNum을 기준으로 정렬해서 조회함 + content가 null일 경우 생기던 nullPointException 해결

--- backend/shareNote/src/main/java/com/Backend/shareNote/domain/Block/service/BlockService.java
+++ backend/shareNote/src/main/java/com/Backend/shareNote/domain/Block/service/BlockService.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -56,6 +58,7 @@ public class BlockService {
         //block에 content 추가 로직
         Block block = blockRepository.findById(contentDTO.getBlockId()).get();
 
+        //Content 만들기
         Block.Content content = Block.Content.builder()
                 .type(contentDTO.getType())
                 .content(contentDTO.getContent())
@@ -66,20 +69,22 @@ public class BlockService {
                 .createUser(contentDTO.getCreateUser())
                 .routingKey(contentDTO.getRoutingKey())
                 .build();
-
+        //Content block에 장착
         block.addContent(content);
 
+        //block 저장
         blockRepository.save(block);
     }
 
     // 페이지에 있는 블록들 조회하는 로직
-    public ResponseEntity<List<BlockDTO>> getBlocks(PageBlocksDTO pageData) {
+    public ResponseEntity<List<BlockDTO>> getBlocks(PageBlocksDTO pageData) { //org, page, note 정보 포함
+        //block 조회 로직
         List<Block> blocks = new ArrayList<>();
         Organization org = organizationRepository.findById(pageData.getOrganizationId()).get();
         org.getNotes().forEach(note -> {
-            if (note.getId().equals(pageData.getNoteId())) {
+            if (note.getId().equals(pageData.getNoteId())) { //노트 발견
                 note.getPages().forEach(page -> {
-                    if (page.getId().equals(pageData.getPageId())) {
+                    if (page.getId().equals(pageData.getPageId())) { // 페이지 발견 -> 블록 ObjectId로 조회 -> list에 때려박아
                         page.getBlocks().forEach(blockId -> {
                             blocks.add(blockRepository.findById(blockId).get());
                         });
@@ -88,37 +93,44 @@ public class BlockService {
                 });
             }
         });
-
+        //왜인지는 모르겠지만 DTO로 변환하는게 필요하다. 그냥 반환하려니까 오류 엄청 나던데??
+        //Serialization이 안된다고 에러 나왔어 아마 block 안에 있는 content가 문제였나봐
         List<BlockDTO> blockDTOs = convertToDTO(blocks);
         return ResponseEntity.ok().body(blockDTOs);
     }
 
+    //blockDTOs를 정렬을 해서 반환해줘야 해!!
     public List<BlockDTO> convertToDTO(List<Block> blocks) {
-        List<BlockDTO> blockDTOs = new ArrayList<>();
-        for (Block block : blocks) {
-            BlockDTO blockDTO = new BlockDTO();
-            blockDTO.setId(block.getId());
-            blockDTO.setType(block.getType());
-            blockDTO.setBlockSeq(block.getBlockSeq());
-            blockDTO.setCreateUser(block.getCreateUser());
+        List<BlockDTO> blockDTOs = blocks.stream().map(block -> {
+                    BlockDTO blockDTO = new BlockDTO();
+                    blockDTO.setId(block.getId());
+                    blockDTO.setType(block.getType());
+                    blockDTO.setBlockSeq(block.getBlockSeq());
+                    blockDTO.setCreateUser(block.getCreateUser());
 
-            List<BlockDTO.ContentDTO> contentDTOs = block.getContent().stream()
-                    .map(content -> {
-                        BlockDTO.ContentDTO contentDTO = new BlockDTO.ContentDTO();
-                        contentDTO.setType(content.getType());
-                        contentDTO.setContent(content.getContent());
-                        contentDTO.setBlockId(content.getBlockId());
-                        contentDTO.setBlockSeq(content.getBlockSeq());
-                        contentDTO.setNickName(content.getNickName());
-                        contentDTO.setCrdtIndex(content.getCrdtIndex());
-                        return contentDTO;
-                    })
-                    .collect(Collectors.toList());
+                    // Content를 DTO로 변환
+                    // 졸라 긴 삼항 연산자
+                    List<BlockDTO.ContentDTO> contentDTOs = block.getContent() != null ? block.getContent().stream()
+                            .map(content -> {
+                                BlockDTO.ContentDTO contentDTO = new BlockDTO.ContentDTO();
+                                contentDTO.setType(content.getType());
+                                contentDTO.setContent(content.getContent());
+                                contentDTO.setBlockId(content.getBlockId());
+                                contentDTO.setBlockSeq(content.getBlockSeq());
+                                contentDTO.setNickName(content.getNickName());
+                                contentDTO.setCrdtIndex(content.getCrdtIndex());
+                                return contentDTO;
+                            })
+                            .collect(Collectors.toList()) : Collections.emptyList();
 
-            blockDTO.setContent(contentDTOs);
-            blockDTOs.add(blockDTO);
-        }
+                    blockDTO.setContent(contentDTOs);
+                    return blockDTO;
+                })
+                .sorted(Comparator.comparingInt(BlockDTO::getBlockSeq)) // BlockSeq를 기준으로 정렬
+                .collect(Collectors.toList());
+
         return blockDTOs;
     }
+
 
 }
