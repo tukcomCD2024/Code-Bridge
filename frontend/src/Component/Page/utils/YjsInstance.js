3f4aa41721ea7f19aab0aadd125d28c8c8842945
Shin Hyuk Jun <tlsgurwns123@naver.com>
2024-03-27 11:12:12 +0900

    feat: yjs 생성시 콘솔 출력 문구 간소화

--- frontend/src/Component/Page/utils/YjsInstance.js
+++ frontend/src/Component/Page/utils/YjsInstance.js
@@ -5,9 +5,7 @@ const ydocs = {};
 export const getYDocInstance = (roomId) => {
   if (!ydocs[roomId]) {
     ydocs[roomId] = new Y.Doc();
-    console.log(`Created new Y.Doc for room: ${roomId}`);
-  } else {
-    console.log(`Using existing Y.Doc for room: ${roomId}`);
   }
+  console.log(`접속: ${roomId}`);
   return ydocs[roomId];
 };
