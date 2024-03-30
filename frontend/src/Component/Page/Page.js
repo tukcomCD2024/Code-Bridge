0cfad6556e9b9d718afeeaaeda6eaa427b8172e4
Shin Hyuk Jun <tlsgurwns123@naver.com>
2024-03-30 13:13:32 +0900

    feat: 노드의 맨 앞에서 노드 분할 시 새로운 UUID값 부여받는 문제점 해결

--- frontend/src/Component/Page/Page.js
+++ frontend/src/Component/Page/Page.js
@@ -108,32 +108,53 @@ const createPlugin = (guidGenerator = uuidv4) => {
         return false; // Return false to indicate that the editor should continue handling the click event
       },
     },
-
     appendTransaction: (transactions, prevState, nextState) => {
       const tr = nextState.tr;
       let modified = false;
-      const generatedIds = new Set(); // 생성된 ID를 추적하기 위한 Set입니다.
-
+      const generatedIds = new Set();
+    
       if (transactions.some(transaction => transaction.docChanged)) {
         const { paragraph } = nextState.schema.nodes;
-        nextState.doc.descendants((node, pos) => {
-          // 기존 guid가 있지만 중복된 경우 또는 guid가 없는 경우 새로운 guid를 생성
-          if (node.type === paragraph) {
-            let currentGuid = node.attrs.guid;
-            // guid가 없거나 이미 생성된 guid Set에 존재하는 경우 새로운 guid 생성
-            if (!currentGuid || generatedIds.has(currentGuid)) {
-              let newGuid;
-              do {
-                newGuid = guidGenerator();
-              } while (generatedIds.has(newGuid)); // 새 guid가 고유할 때까지 반복
+        let prevNode = null; // 이전 노드를 추적하기 위한 변수
+        let prevPos = null; // 이전 노드의 위치를 저장
     
-              generatedIds.add(newGuid); // 새로운 guid를 Set에 추가
-              tr.setNodeMarkup(pos, undefined, {...node.attrs, guid: newGuid});
-              modified = true;
-            } else {
-              // 기존 guid가 고유하면 Set에 추가 (중복 체크를 위함)
-              generatedIds.add(currentGuid);
+        nextState.doc.descendants((node, pos) => {
+          if (node.type === paragraph) {
+            const nodeTextContent = node.textContent;
+            const selection = nextState.selection;
+            const cursorPosition = selection.head || selection.from;
+    
+            if (cursorPosition >= pos && cursorPosition <= pos + node.nodeSize) {
+              const cursorPositionInNode = cursorPosition - pos;
+              if (cursorPositionInNode === 1 && nodeTextContent !== "") {
+                if (prevNode && !generatedIds.has(prevNode.attrs.guid)) {
+                  let newGuid;
+                  do {
+                    newGuid = guidGenerator();
+                  } while (generatedIds.has(newGuid));
+                  generatedIds.add(newGuid);
+                  tr.setNodeMarkup(prevPos, undefined, {...prevNode.attrs, guid: newGuid});
+                  modified = true;
+                }
+              } else {
+                // 일반적인 guid 할당 로직
+                let currentGuid = node.attrs.guid;
+                if (!currentGuid || generatedIds.has(currentGuid)) {
+                  let newGuid;
+                  do {
+                    newGuid = guidGenerator();
+                  } while (generatedIds.has(newGuid));
+                  generatedIds.add(newGuid);
+                  tr.setNodeMarkup(pos, undefined, {...node.attrs, guid: newGuid});
+                  modified = true;
+                } else {
+                  generatedIds.add(currentGuid);
+                }
+              }
             }
+            // 현재 노드와 위치를 이전 노드로 업데이트
+            prevNode = node;
+            prevPos = pos;
           }
         });
       }
