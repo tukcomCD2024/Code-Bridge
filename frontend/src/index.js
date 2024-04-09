8d21896132fb191d55224ffc3195f2cf436f61fd
Shin Hyuk Jun <tlsgurwns123@naver.com>
2024-04-10 00:57:55 +0900

    외부 폰트 적용 코드 수정
    styles.css 및 index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import { createGlobalStyle } from "styled-components";
 
 import App from "./App";
+import "./styles.css";
 
 const GlobalFontStyle = createGlobalStyle`
+    @import url("https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css");
+    @import url("https://cdn.jsdelivr.net/gh/toss/tossface/dist/tossface.css");
+
   * {
     font-family: "Spoqa Han Sans Neo", "Tossface","sans-serif";
   }
