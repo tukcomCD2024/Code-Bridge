34db12981e37c38a9e8ba2bd90eecdcedec94eea
Shin Hyuk Jun <tlsgurwns123@naver.com>
2024-04-17 18:28:20 +0900

    CSS 수정

--- frontend/src/Component/Page/utils/noteSettingModal.js
+++ frontend/src/Component/Page/utils/noteSettingModal.js
@@ -218,8 +218,9 @@ const LeftInsideContainer = styled(HalfContainer)`
   background-color: rgba(250, 190, 88, 0.2); 
 
   img{
-    width: 100%;
+    width: auto;
     height: auto;
+    max-width: 100%;
     max-height: 180px;
   }
 `;
