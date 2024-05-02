d63c2b31698519fb906fce9f9ec172c2da9c5abb
Kimsangwon <doncham98@gmail.com>
2024-05-02 13:53:05 +0900

    feat : brush 색상 선택 코드 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -87,6 +87,9 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
+    implementation ("com.github.Dhaval2404:ColorPicker:2.3")
+
+
 
 
 }
