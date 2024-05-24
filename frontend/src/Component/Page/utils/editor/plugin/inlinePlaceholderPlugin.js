6c9500114dc506fafa7242d67176efc4355cd0e3
Shin Hyuk Jun <tlsgurwns123@naver.com>
2024-05-25 00:19:05 +0900

    placeholder 로직
    새로운 유저 접속 시 새로운 줄 추가 안되도록 수정

--- frontend/src/Component/Page/utils/editor/plugin/inlinePlaceholderPlugin.js
+++ frontend/src/Component/Page/utils/editor/plugin/inlinePlaceholderPlugin.js
@@ -1,40 +1,47 @@
-import { Plugin } from "prosemirror-state";
-import { Decoration, DecorationSet } from "prosemirror-view";
+import { Plugin } from 'prosemirror-state';
+import { Decoration, DecorationSet } from 'prosemirror-view';
 
 // 노트 페이지에서 블록(노드)가 비어있으면 "내용을 입력하세요..." placeholder를 표시한다.
 export function inlinePlaceholderPlugin() {
   return new Plugin({
+    state: {
+      init: (_, { doc }) => {
+        return DecorationSet.create(doc, []);
+      },
+      apply: (tr, old) => {
+        const decorations = findPlaceholders(tr.doc, tr.selection);
+        return DecorationSet.create(tr.doc, decorations);
+      }
+    },
     props: {
       decorations(state) {
-        const decorations = [];
-        const { doc, selection } = state;
-
-        doc.descendants((node, pos) => {
-          if (!node.isBlock || !!node.textContent) return;
-          if (selection.empty && selection.from === pos + 1) {
-            // The selection is inside the node
-            if (node.type.name === "paragraph") {
-              decorations.push(
-                Decoration.node(pos, pos + node.nodeSize, {
-                  class: "placeholder",
-                  'data-placeholder': '내용을 입력하세요...'
-                })
-              );
-            }
-          } 
-          // else if (node.type.name !== "paragraph") {
-          //   decorations.push(
-          //     Decoration.node(pos, pos + node.nodeSize, {
-          //       class: "placeholder non-paragraph",
-          //       'data-placeholder': `${node.type.name}_작성자 이름`
-          //     })
-          //   );
-          // }
-          return false;
-        });
-
-        return DecorationSet.create(doc, decorations);
-      },
-    },
+        return this.getState(state);
+      }
+    }
   });
+}
+
+function findPlaceholders(doc, selection) {
+  const decorations = [];
+  const { from } = selection;
+
+  doc.descendants((node, pos) => {
+    if (!node.isBlock || node.content.size > 0) return false;
+
+    const isCursorInsideNode = selection.empty && (from >= pos && from <= pos + node.nodeSize);
+    
+    // Only add placeholder if the node is empty and the cursor is inside the node
+    if (node.type.name === "paragraph" && isCursorInsideNode) {
+      decorations.push(
+        Decoration.node(pos, pos + node.nodeSize, {
+          class: "placeholder",
+          'data-placeholder': '내용을 입력하세요...'
+        })
+      );
+    }
+    
+    return false;
+  });
+
+  return decorations;
 }
