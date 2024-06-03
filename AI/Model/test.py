01cd38c6db9015ffa116328c5abe6b3e40decc58
가인철 <45278265+entellaKa@users.noreply.github.com>
2024-06-04 08:12:40 +0900

    multiple ai

--- AI/Model/test.py
+++ AI/Model/test.py
@@ -1,4 +1,5 @@
 import os
+import operator
 import numpy as np
 from PIL import Image
 from keras.models import load_model
@@ -10,7 +11,6 @@ def resultByDesc(result):
         if result[i] > 0.001:
             x[i] = result[i]
 
-    import operator
     return sorted(x.items(), key=operator.itemgetter(1), reverse=True)[:40]
 
 
@@ -19,17 +19,31 @@ def resultByDesc(result):
 imgsrc = r"C:\Users\Ka\Desktop\Ka\programming\AI\sample2\{}.png"
 images = {'heart': 54, 'heart2': 54, 'cloud': 31, 'sword': 96, 'skirt': 86, 'star': 92, 'star2': 92, 'bread': 15, 'bread2': 15, 'candy': 24,
           'flask': 47, 'dice': 37, 'dice2': 37, 'house': 56, 'house2': 56, 'cylinder2': 36, 'cylinder3': 36, 'school': 83, 'school2': 83, }
+modellist = ("cnn5e58v2.h5,cnn5e59v3.h5,cnn5e80vc.h5,cnn5f32e60u1024.h5,cnn6e40vc.h5,cnn6e47v2.h5,cnn6e50v3.h5,cnn6e50vc.h5,cnn6e51v2.h5,cnn6e57v2.h5,cnn6e5" +
+             "9v2.h5,cnn6e60v3.h5,cnn6e61v3.h5,cnn6e62v2.h5,cnn6e63v2.h5,cnn6e63v3.h5,cnn6e65v3.h5,cnn6e66v2.h5,cnn6e67v2.h5,cnn6e68v2.h5,cnn6e80v3.h5").split(
+    ',')
+
+
+def allModels():
+    models = []
+    for m in modellist:
+        models.append(load_model(m))
+    return models
 
 
 def imagePredictMono():
-    for i in os.listdir('./'):
+    for i in modellist:
         if not '.h5' in i:
             continue
         print(i)
 
-        saved_model = load_model(i)
+        saved_model = load_model("./" + i)
 
+        total = 0
+        correct = 0
         for key in images.keys():
+            total += 40
+
             img = Image.open(imgsrc.format(key))
             img = img.resize((128, 128))
             img = img.convert("RGB")
@@ -38,15 +52,21 @@ def imagePredictMono():
 
             output = saved_model.predict(img)
 
-            result = resultByDesc(output[0])[:8]
-            for j in range(5):
+            result = resultByDesc(output[0])[:40]
+            for j in range(40):
                 try:
                     if images[key] in result[j]:
-                        print(key, j)
+                        # print(key, j)
+                        total += j
+                        total -= 40
+                        correct += 1
                         break
                 except:
-                    print("outOfBound")
+                    # print("outOfBound")
                     break
+
+        print("total:", total)
+        # print("correct:", correct)
 
 
 def imagePredictPoly(models):
@@ -57,17 +77,28 @@ def imagePredictPoly(models):
         img = np.array(img)
         img = np.expand_dims(img, axis=0)
 
-        total = {}
+        total1 = {}
+        total2 = {}
 
         for m in models:
-           result = resultByDesc(m.predict(img)[0])[:6]
-           for r in result:
-               if r in total.keys():
-                   total[r] += 1
-               else:
-                   total[r] = 1
-        result = resultByDesc(result)
+            predict = m.predict(img)[0]
+            result = resultByDesc(predict)[:40]
+            for r in result:
+                i = r[0]
+                if i in total1.keys():
+                    total1[i] += 1
+                    total2[i] += result.index(r)
+                else:
+                    total1[i] = 1
+                    total2[i] = result.index(r)
+        # result = resultByDesc(result)
 
-        print(result)
+        result1 =  sorted(total1.items(), key=operator.itemgetter(1), reverse=True)[:40]
+        result2 =  sorted(total2.items(), key=operator.itemgetter(1), reverse=True)[:40]
+        print(key, images[key])
+        print(result1)
+        print(result2)
 
-imagePredictMono()
+
+# imagePredictMono()
+imagePredictPoly(allModels())
