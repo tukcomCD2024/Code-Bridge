909e8d821210bca91cf0eca83cc4ecbe8f9de8ad
Ko SeongChan <shinjineunsu@gmail.com>
2024-06-18 21:10:56 +0900

    기여도

--- android/app/src/main/java/com/example/sharenote/Contribution.kt
+++ android/app/src/main/java/com/example/sharenote/Contribution.kt
@@ -20,7 +20,6 @@ class Contribution : AppCompatActivity() {
                 WebChromeClient() // 크롬환경에 맞는 세팅을 해줌. 특히, 알람등을 받기위해서는 꼭 선언해주어야함 (alert같은 경우)
             settings.javaScriptEnabled = true // 자바스크립트 허용
             settings.javaScriptCanOpenWindowsAutomatically = false
-            // 팝업창을 띄울 경우가 있는데, 해당 속성을 추가해야 window.open() 이 제대로 작동 , 자바스크립트 새창도 띄우기 허용여부
             settings.setSupportMultipleWindows(false) // 새창 띄우기 허용 여부 (멀티뷰)
             settings.loadsImagesAutomatically = true // 웹뷰가 앱에 등록되어 있는 이미지 리소스를 자동으로 로드하도록 설정하는 속성
             settings.useWideViewPort = true // 화면 사이즈 맞추기 허용 여부
@@ -29,11 +28,8 @@ class Contribution : AppCompatActivity() {
             settings.builtInZoomControls = false // 화면 확대 축소 허용여부
             settings.displayZoomControls = false // 줌 컨트롤 없애기.
             settings.cacheMode = WebSettings.LOAD_NO_CACHE // 웹뷰의 캐시 모드를 설정하는 속성으로써 5가지 모드
-
-
-            settings.domStorageEnabled =
-                true // 로컬 스토리지 사용 여부를 설정하는 속성으로 팝업창등을 '하루동안 보지 않기' 기능 사용에 필요
-            settings.allowContentAccess // 웹뷰 내에서 파일 액세스 활성화 여부
+            settings.domStorageEnabled = true // 로컬 스토리지 사용 여부를 설정하는 속성으로 팝업창등을 '하루동안 보지 않기' 기능 사용에 필요
+            settings.allowContentAccess = true // 웹뷰 내에서 파일 액세스 활성화 여부
             settings.userAgentString = "app" // 웹에서 해당 속성을 통해 앱에서 띄운 웹뷰로 인지 할 수 있도록 합니다.
             settings.defaultTextEncodingName = "UTF-8" // 인코딩 설정
             settings.databaseEnabled = true //Database Storage API 사용 여부 설정
